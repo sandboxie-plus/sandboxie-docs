dc8bbf16343466e405dac98ab9688ef51d52ffb3
QZLin <qzlin01@163.com>
2024-09-08 19:32:12 +0800

    incorporate util powershell script

--- util/SBIE_list.ps1
+++
@@ -1,7 +0,0 @@
-Push-Location $PSScriptRoot/..
-$space_len = 4
-$c = Get-ChildItem "docs/Content" -Filter "SBIE*" | `
-    ForEach-Object { "$( ' '*$space_len )- Content/$( $_.BaseName ).md" }
-#Set-Clipboard -Value $c
-Write-Output $c
-Pop-Location
